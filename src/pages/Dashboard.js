48effb73912a731b8c9df480a0db5005a57531f3
bhadresh <bhadresh.lamprostech@gmail.com>
2023-02-15 18:17:43 +0530

    FrontEnd

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -80,7 +80,7 @@ function Dashboard() {
               dashboardLinks("Dashboard");
             }}
           >
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               enable-background="new 0 0 24 24"
               height="24px"
@@ -99,7 +99,7 @@ function Dashboard() {
                   <path d="M15,21h4c1.1,0,2-0.9,2-2v-4c0-1.1-0.9-2-2-2h-4c-1.1,0-2,0.9-2,2v4C13,20.1,13.9,21,15,21z" />
                 </g>
               </g>
-            </svg>
+            </svg> */}
             Dashboard
           </li>
           <li
@@ -108,7 +108,7 @@ function Dashboard() {
               dashboardLinks("Proposals");
             }}
           >
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               enable-background="new 0 0 24 24"
               height="24px"
@@ -127,7 +127,7 @@ function Dashboard() {
                   <path d="M15,21h4c1.1,0,2-0.9,2-2v-4c0-1.1-0.9-2-2-2h-4c-1.1,0-2,0.9-2,2v4C13,20.1,13.9,21,15,21z" />
                 </g>
               </g>
-            </svg>
+            </svg> */}
             Proposals
           </li>
           <li
@@ -136,7 +136,7 @@ function Dashboard() {
               dashboardLinks("Tokens");
             }}
           >
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               enable-background="new 0 0 24 24"
               height="24px"
@@ -146,7 +146,7 @@ function Dashboard() {
             >
               <rect fill="none" height="24" width="24" y="0" />
               <path d="M12.97,2.54c-0.6-0.34-1.34-0.34-1.94,0l-7,3.89L9.1,9.24C9.83,8.48,10.86,8,12,8s2.17,0.48,2.9,1.24l5.07-2.82L12.97,2.54z M10,12c0-1.1,0.9-2,2-2s2,0.9,2,2s-0.9,2-2,2S10,13.1,10,12z M3,8.14l5.13,2.85C8.04,11.31,8,11.65,8,12c0,1.86,1.27,3.43,3,3.87 v5.57l-6.97-3.87C3.39,17.22,3,16.55,3,15.82V8.14z M13,21.44v-5.57c1.73-0.44,3-2.01,3-3.87c0-0.35-0.04-0.69-0.13-1.01L21,8.14 l0,7.68c0,0.73-0.39,1.4-1.03,1.75L13,21.44z" />
-            </svg>
+            </svg> */}
             Tokens
           </li>
           <li
@@ -155,7 +155,7 @@ function Dashboard() {
               dashboardLinks("DataDAOs");
             }}
           >
-            <svg
+            {/* <svg
               xmlns="http://www.w3.org/2000/svg"
               height="24px"
               viewBox="0 0 24 24"
@@ -164,7 +164,7 @@ function Dashboard() {
             >
               <path d="M0 0h24v24H0V0z" fill="none" />
               <path d="M19 13H5c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2v-4c0-1.1-.9-2-2-2zM7 19c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2zM19 3H5c-1.1 0-2 .9-2 2v4c0 1.1.9 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zM7 9c-1.1 0-2-.9-2-2s.9-2 2-2 2 .9 2 2-.9 2-2 2z" />
-            </svg>
+            </svg> */}
             DataDAOs
           </li>
         </ul>
